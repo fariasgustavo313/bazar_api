8bcb4212792a411ab4253278e9f566ed63a1ad92
Gustavo <farias.gustavoe@gmail.com>
2025-07-02 03:35:31 -0300

    Implementacion de ClienteService

--- src/main/java/com/example/project_spring_boot/repository/ClienteRepository.java
+++ src/main/java/com/example/project_spring_boot/repository/ClienteRepository.java
@@ -1,8 +1,9 @@
 package com.example.project_spring_boot.repository;
 
+import com.example.project_spring_boot.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Venta extends JpaRepository<Venta,Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
--- src/main/java/com/example/project_spring_boot/repository/VentaRepository.java
+++ src/main/java/com/example/project_spring_boot/repository/VentaRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Cliente extends JpaRepository<Cliente, Long> {
+public interface VentaRepository extends JpaRepository<VentaRepository,Long> {
 }
