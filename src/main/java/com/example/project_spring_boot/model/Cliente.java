646b48992bf89c70ca9e08124e1d6987a12dc333
Gustavo <farias.gustavoe@gmail.com>
2025-07-02 03:12:04 -0300

    Modelado de clase Venta

---
+++ src/main/java/com/example/project_spring_boot/model/Cliente.java
@@ -0,0 +1,5 @@
+package com.example.project_spring_boot.model;
+
+
+public class Cliente {
+}
---
+++ src/main/java/com/example/project_spring_boot/model/Venta.java
@@ -0,0 +1,27 @@
+package com.example.project_spring_boot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "ventas")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Venta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate fecha_venta;
+    private double total;
+    private List<Producto> productos;
+    private Cliente cliente;
+}
