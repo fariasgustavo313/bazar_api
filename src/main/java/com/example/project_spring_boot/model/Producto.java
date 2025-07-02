aa3d9fdeaaedd1ad6778b017f3afa42a17385b26
Gustavo <farias.gustavoe@gmail.com>
2025-07-02 03:09:36 -0300

    Modelado de la clase Producto

---
+++ src/main/java/com/example/project_spring_boot/model/Producto.java
@@ -0,0 +1,24 @@
+package com.example.project_spring_boot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "productos")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String marca;
+    private double costo;
+    private int cantidad_disponible;
+}
