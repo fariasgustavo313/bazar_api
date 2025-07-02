0ffa139cb5b48e488d1cabfe7b84d43993397b05
Gustavo <farias.gustavoe@gmail.com>
2025-07-02 03:13:24 -0300

    Modelado de clase Cliente

--- src/main/java/com/example/project_spring_boot/model/Cliente.java
+++ src/main/java/com/example/project_spring_boot/model/Cliente.java
@@ -1,5 +1,23 @@
 package com.example.project_spring_boot.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "clientes")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String dni;
 }
