9337c885e9c3ac442ebeaea70b60ae77a683074a
Gustavo <farias.gustavoe@gmail.com>
2025-07-05 18:27:57 -0300

    Validaciones en ClienteService

--- src/main/java/com/example/project_spring_boot/controller/ClienteController.java
+++ src/main/java/com/example/project_spring_boot/controller/ClienteController.java
@@ -3,8 +3,10 @@ package com.example.project_spring_boot.controller;
 import com.example.project_spring_boot.model.Cliente;
 import com.example.project_spring_boot.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,23 +17,25 @@ public class ClienteController {
     private ClienteService clienteService;
 
     @GetMapping
-    public List<Cliente> obtenerClientes(){
-        return clienteService.obtenerClientes();
+    public ResponseEntity<List<Cliente>> obtenerClientes(){
+        return ResponseEntity.ok(clienteService.obtenerClientes());
     }
 
     @GetMapping("/{id}")
-    public Cliente obtenerClientePorId(@PathVariable Long id){
-        return clienteService.obtenerClientePorId(id);
+    public ResponseEntity<Cliente> obtenerClientePorId(@PathVariable Long id){
+        return ResponseEntity.ok(clienteService.obtenerClientePorId(id));
     }
 
     @PostMapping
-    public void crearCliente(@RequestBody Cliente cliente){
+    public ResponseEntity<Cliente> crearCliente(@RequestBody Cliente cliente){
         clienteService.crearCliente(cliente);
+        return ResponseEntity.created(URI.create("/clientes/"+cliente.getId())).body(cliente);
     }
 
     @DeleteMapping("/{id}")
-    public void eliminarCliente(@PathVariable Long id){
+    public ResponseEntity<Cliente> eliminarCliente(@PathVariable Long id){
         clienteService.eliminarCliente(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
